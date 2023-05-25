51b22420889a5c06b6f6546f3222bb96634d5bd9
francis <franzdiaz460@gmail.com>
2023-05-25 12:25:17 +0400

    ref: installed tailwind

---
+++ src/components/AuthContents.tsx
@@ -0,0 +1,5 @@
+const AuthContents = ({ action }: { action: string }) => {
+	return <div id="auth-contents"></div>;
+};
+
+export default AuthContents;
--- src/pages/RootAuth.tsx
+++ src/pages/RootAuth.tsx
@@ -1,3 +1,4 @@
+import AuthContents from "@/components/AuthContents";
 import AuthForm from "@/components/AuthForm";
 import { useParams, NavLink, useLoaderData } from "react-router-dom";
 
@@ -5,7 +6,9 @@ const RootAuth = () => {
 	const { action } = useLoaderData();
 	return (
 		<main id="auth" className="page">
-			<section id="left-auth-contents">{/* {currentParameter} */}</section>
+			<section id="left-auth-contents">
+				<AuthContents action={action} />
+			</section>
 			<section id="auth-form">
 				<AuthForm action={action} />
 			</section>
