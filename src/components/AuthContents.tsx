6e21a268fb2785ae0d22f2f64ad2730d95648c8b
francis-lp <franzdiaz460@gmail.com>
2023-05-25 16:42:33 +0400

    style: add basic style for text contents on left content

--- src/components/AuthContents.tsx
+++ src/components/AuthContents.tsx
@@ -1,9 +1,31 @@
-const AuthContents = ({ action }: { action: string }) => {
+import { Link } from "react-router-dom";
+
+const AuthContents = ({ action }: { action: "/" | "/sign-up" }) => {
 	return (
 		<div
 			id="auth-contents"
 			className="bg-auth-left-waves bg-cover bg-no-repeat bg-center w-full"
-		></div>
+		>
+			<div className="auth-text-container w-[30%]">
+				<h1 className="text-7xl font-bold border-b-2 border-black border-solid inline-block">
+					# Hi There!
+				</h1>
+				<div className="greet">
+					<p className="text-4xl">
+						Welcome to <span className="font-bold">Vimnotes</span>
+					</p>
+					<p className="text-2xl font-light">
+						A simple markdown app with vim key mappings.
+					</p>
+				</div>
+				<Link
+					to={action === "/" ? "/sign-up" : "/"}
+					className="font-bold text-[#ffffff] bg-vn-blue px-10 py-4 text-lg inline-block"
+				>
+					Create account
+				</Link>
+			</div>
+		</div>
 	);
 };
 
