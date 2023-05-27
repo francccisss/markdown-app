7eb196c6d3bbf2cd341adce42fde013dc94ca337
francis <franzdiaz460@gmail.com>
2023-05-28 03:32:40 +0400

    ref: add syntax highlighter

--- src/components/preview/Preview.tsx
+++ src/components/preview/Preview.tsx
@@ -2,6 +2,8 @@ import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import "./preview.scss";
 import "github-markdown-css";
 import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { oneDark as previewStyle } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 interface IPreviewProps {
 	markdownInput: string;
@@ -10,7 +12,30 @@ interface IPreviewProps {
 const Preview = ({ markdownInput }: IPreviewProps) => {
 	return (
 		<section id="markdown-preview" className="markdown-body px-4">
-			<ReactMarkdown children={markdownInput} remarkPlugins={[remarkGfm]} />
+			<ReactMarkdown
+				children={markdownInput}
+				remarkPlugins={[remarkGfm]}
+				components={{
+					code({ node, inline, className, children, ...props }) {
+						const match = /language-(\w+)/.exec(className || "");
+						return !inline && match ? (
+							<SyntaxHighlighter
+								{...props}
+								// useInlineStyles={false}
+								className={"syntax-highlighter"}
+								children={String(children).replace(/\n$/, "")}
+								style={previewStyle}
+								language={match[1]}
+								PreTag="div"
+							/>
+						) : (
+							<code {...props} className={className}>
+								{children}
+							</code>
+						);
+					},
+				}}
+			/>
 		</section>
 	);
 };
