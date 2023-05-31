801851ced28d39ec9bb19e055b25902ffe43f4c0
francis-lp <franzdiaz460@gmail.com>
2023-05-31 11:50:43 +0400

    ref: add placeholders for noteitem

--- src/components/NoteItem.tsx
+++ src/components/NoteItem.tsx
@@ -15,11 +15,15 @@ const NoteItem = ({ note }: INoteItemProps) => {
 			id={note.id}
 			className="note min-h-[50px] block pl-5 py-3 mb-3 box-content hover:bg-vn-dshade-white3 hover:border-l-vn-blue hover:border-l-4 select-none cursor-pointer transition-all ease-in-out duration-100  text-vn-white max-w-full border-vn-outline-black border-l-[2px] "
 		>
-			<p className="block box-content max-w-[384px] overflow-hidden whitespace-nowrap overflow-ellipsis font-semibold uppercase">
-				{note.contents.slice(0, characterLimit)}
+			<p className="inline-block box-content max-w-[210px] overflow-hidden whitespace-nowrap overflow-ellipsis font-semibold ">
+				{note.contents === ""
+					? "Blank"
+					: note.contents.slice(0, characterLimit)}
 			</p>
 			<p className="text-vn-outline-black box-border max-w-[380px] overflow-hidden whitespace-nowrap overflow-ellipsis block">
-				{note.contents.slice(characterLimit)}
+				{note.contents === ""
+					? "no contents..."
+					: note.contents.slice(characterLimit)}
 			</p>
 		</NavLink>
 	);
