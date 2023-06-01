d27aa20136f3af39702feaf22d18614fc6aeb06f
francis-lp <franzdiaz460@gmail.com>
2023-06-01 10:30:55 +0400

    style: imported new icons and changed bulky size of navbar to a more linear look

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,12 +14,12 @@ const Navbar = ({ sideBarRef }) => {
 	return (
 		<nav
 			id="navbar"
-			className="min-h-[40px] bg-no-repeat flex bg-vn-black px-6 py-2 items-center border-separator-grey-line z-20"
+			className="min-h-[35px] bg-no-repeat flex bg-vn-black px-6 py-2 items-center border-separator-grey-line z-20"
 		>
 			<div id="left" className="flex ">
 				<button
 					onClick={sideBarActivitiy}
-					className="btn-actions w-[30px] h-[30px] bg-center"
+					className="btn-actions w-[25px] h-[25px] bg-center"
 				>
 					<svg
 						viewBox="0 0 34 34"
@@ -54,7 +54,7 @@ const Navbar = ({ sideBarRef }) => {
 				<Link
 					id="vim-cheatsheet"
 					to={"/app/info/vim-shortcuts"}
-					className="btn-actions w-[25px] h-[25px] bg-center inline-block mr-5"
+					className="btn-actions w-[25px] h-[25px] bg-center inline-block mr-2"
 				>
 					<svg
 						viewBox="0 0 34 34"
@@ -62,48 +62,22 @@ const Navbar = ({ sideBarRef }) => {
 						xmlns="http://www.w3.org/2000/svg"
 					>
 						<path
-							d="M0 0H33.9V33.9H0V0Z"
-							fill="white"
-							fill-opacity="0.01"
-						/>
-						<path
-							d="M7.29699 3.64849C7.29699 2.86839 7.92937 2.23599 8.70949 2.23599H28.4845C29.2646 2.23599 29.897 2.86839 29.897 3.64849V29.0735C29.897 29.8536 29.2646 30.486 28.4845 30.486H8.70949C7.92937 30.486 7.29699 29.8536 7.29699 29.0735V3.64849Z"
+							d="M5.64999 4.23751C5.64999 3.45741 6.28239 2.82501 7.06249 2.82501H21.1875L28.25 9.88751V29.6625C28.25 30.4426 27.6176 31.075 26.8375 31.075H7.06249C6.28239 31.075 5.64999 30.4426 5.64999 29.6625V4.23751Z"
 							stroke="#878A8C"
+							stroke-width="2"
 							stroke-linejoin="round"
 						/>
 						<path
-							d="M24.247 3.6485V29.0735"
+							d="M11.3 14.125H22.6"
 							stroke="#878A8C"
+							stroke-width="2"
 							stroke-linecap="round"
 							stroke-linejoin="round"
 						/>
 						<path
-							d="M4.47198 9.29852H10.122"
+							d="M11.3 19.775H22.6"
 							stroke="#878A8C"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-						/>
-						<path
-							d="M4.47198 16.361H10.122"
-							stroke="#878A8C"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-						/>
-						<path
-							d="M4.47198 23.4235H10.122"
-							stroke="#878A8C"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-						/>
-						<path
-							d="M19.3032 2.23599H27.7782"
-							stroke="#878A8C"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-						/>
-						<path
-							d="M19.3032 30.486H27.7782"
-							stroke="#878A8C"
+							stroke-width="2"
 							stroke-linecap="round"
 							stroke-linejoin="round"
 						/>
@@ -111,7 +85,7 @@ const Navbar = ({ sideBarRef }) => {
 				</Link>
 				<button
 					id="note-actions"
-					className="btn-actions w-[30px] h-[30px] inline-block"
+					className="btn-actions w-[25px] h-[25px] inline-block"
 				>
 					<svg
 						viewBox="0 0 34 34"
--- src/components/SidebarActions.tsx
+++ src/components/SidebarActions.tsx
@@ -12,7 +12,7 @@ const SidebarActions = ({
 	return (
 		<div
 			id="sidebar-actions"
-			className="p-3 shadowgap-2 flex items-center mb-3 border-b-[3px]  border-b-separator-grey-line"
+			className="p-3 shadow gap-2 flex items-center mb-3 border-b-[3px]  border-b-separator-grey-line"
 		>
 			<input
 				className="w-[100%] shadow-inner hover:outline-vn-blue px-4 py-2 block text-sm placeholder:text-center placeholder:text-vn-outline-black outline-none border-none text-[#d9d9d9] outline-1 rounded outline transition-all duration-150 ease-in-out  focus-within:outline-vn-blue bg-vn-dshade-black"
