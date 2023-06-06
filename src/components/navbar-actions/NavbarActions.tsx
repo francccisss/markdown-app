4be727cd811d97774a404b7aec032dc4e6df1ea1
francis <franzdiaz460@gmail.com>
2023-06-06 17:10:03 +0400

    ref: navbar actions

--- src/components/navbar-actions/NavbarActions.tsx
+++ src/components/navbar-actions/NavbarActions.tsx
@@ -1,14 +1,19 @@
 const NavbarActions = () => {
+	// separate actions
 	const actions = {
 		info: "Note info",
 		save: "Save note",
 		removeNote: "Remove note",
 	};
 
+	// same as vimnoteCheatSheet
 	const mapActions = Object.values(actions).map((action) => {
 		return (
-			<li key={action} className="block border-b-vn-dshade-white border-b">
-				<button className="px-2 w-full text-left " key={action}>
+			<li key={action} className="border-b-vn-dshade-white block">
+				<button
+					className="px-2 py-3 transition-all duration-100 ease-in-out hover:bg-vn-dshade-white3 w-full text-left "
+					key={action}
+				>
 					{action}
 				</button>
 			</li>
@@ -18,9 +23,9 @@ const NavbarActions = () => {
 	return (
 		<div
 			id="navbar-actions"
-			className="absolute right-4 z-0 top-[50px] text-vn-white rounded border-vn-blue border-2"
+			className="absolute right-4 z-0 top-[50px] text-[.9rem] text-vn-white bg-vn-black shadow-lg rounded border-vn-dshade-white border-2"
 		>
-			<ul className="py-2">{mapActions}</ul>
+			<ul className="w-52">{mapActions}</ul>
 		</div>
 	);
 };
