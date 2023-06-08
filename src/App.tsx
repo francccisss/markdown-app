f1e1608a5fa0060c01dc6d4affb69b9a1d41921b
francis <franzdiaz460@gmail.com>
2023-06-08 20:59:00 +0400

    removed analytics

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
