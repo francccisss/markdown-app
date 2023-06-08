5a7397682d62dff2e4953ca4dc90ee24edf4e687
francis <franzdiaz460@gmail.com>
2023-06-08 20:13:08 +0400

    ref: validating password confirmation

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -5,18 +5,26 @@ import { FirebaseContext } from "@/App";
 const SignUp = () => {
 	const [error, setError] = useState<string>("");
 	const { db, auth } = useContext(FirebaseContext);
+
 	async function signUpUser(
 		e: React.InvalidEvent<HTMLFormElement>
 	): Promise<void> {
 		e.preventDefault();
 		const formValidation = e.target;
-		if (formValidation.checkValidity()) {
-			const form = new FormData(e.currentTarget);
-			const formEntries = Object.fromEntries(form.entries());
-			console.log(formEntries);
+		const form = new FormData(e.currentTarget);
+		const formEntries = Object.fromEntries(form.entries());
+		console.log(formEntries);
+		if (
+			formValidation.checkValidity() &&
+			formEntries.password === formEntries.passConf
+		) {
 			console.log("signed up");
-		} else {
-			setError("* Please check your email and password");
+		}
+		if (!formValidation.checkValidity()) {
+			setError("*Please check your email and password");
+		}
+		if (formEntries.password !== formEntries.passConf) {
+			setError("*Password doesn't match");
 		}
 	}
 	return (
