f019bbba484284796bd9806241e0a0940b6719da
francis-lp <franzdiaz460@gmail.com>
2023-06-14 13:26:33 +0400

    ref: add loading screen spinner whil fetching data

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,7 +1,12 @@
 const LoadingScreen = () => {
 	return (
-		<div id="loading-screen" className="">
-			<div className="">LOADING</div>
+		<div
+			id="loading-screen"
+			className="bg-vn-black text-vn-white h-screen flex"
+		>
+			<div className="flex-1 flex items-center justify-center">
+				<span id="loading-spinner" className="loading-spinner"></span>
+			</div>
 		</div>
 	);
 };
