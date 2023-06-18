b5f7030b7b5534eab42bcc3fa920c22daa72cdeb
francis <franzdiaz460@gmail.com>
2023-06-18 06:15:01 +0400

    ref: change font family for uniformity

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -4,12 +4,21 @@ import { vim } from "@replit/codemirror-vim";
 import { markdownLanguage, markdown } from "@codemirror/lang-markdown";
 import { languages } from "@codemirror/language-data";
 import "./editor.scss";
+import createTheme from "@uiw/codemirror-themes";
 
 export interface IEdtiorProps {
 	onChange: (value: string) => void;
 	input: string;
 	newWidth?: number;
 }
+
+const theme = createTheme({
+	theme: "dark",
+	styles: [],
+	settings: {
+		fontFamily: "inter",
+	},
+});
 
 const Editor = ({ onChange, input, newWidth }: IEdtiorProps) => {
 	return (
@@ -18,9 +27,10 @@ const Editor = ({ onChange, input, newWidth }: IEdtiorProps) => {
 			value={input}
 			onChange={onChange}
 			width={`${newWidth}px`}
-			className="focus-within:border-t-[3px] focus-within:border-vn-blue border-solid outline-none"
+			className="focus-within:border-t-[3px] text-sm focus-within:border-vn-blue border-solid outline-none"
 			height="100%"
 			extensions={[
+				theme,
 				vim(),
 				markdown({
 					base: markdownLanguage,
@@ -31,10 +41,6 @@ const Editor = ({ onChange, input, newWidth }: IEdtiorProps) => {
 			basicSetup={{
 				foldGutter: false,
 				highlightActiveLine: false,
-			}}
-			onCreateEditor={(view, state) => {
-				// console.log(state.doc);
-				// console.log("vim");
 			}}
 		/>
 	);
