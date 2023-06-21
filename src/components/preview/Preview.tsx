d4c0fd8f54397731c5e7849b5f4be6f0784b9311
francis <franzdiaz460@gmail.com>
2023-06-21 04:47:56 +0400

    mend

--- src/components/preview/Preview.tsx
+++ src/components/preview/Preview.tsx
@@ -13,12 +13,12 @@ const Preview = ({ markdownInput }: IPreviewProps) => {
 	return (
 		<section
 			id="markdown-preview"
-			className={`@container overflow-auto h-full px-16 py-5 z-10 markdown-body flex-1 w-0 flex justify-center  `}
+			className={`@container overflow-auto  px-16  py-10 z-10 markdown-body box-border flex-1 w-0 flex justify-center `}
 		>
 			<ReactMarkdown
 				children={markdownInput}
 				remarkPlugins={[remarkGfm]}
-				className="z-10 react-markdown @[200px]:min-w-[200px] @[700px]:w-[800px] @[1000px]:w-[1000px]"
+				className="z-10 react-markdown h-fit my-5 @[200px]:min-w-[200px] @[700px]:w-[800px] @[1000px]:w-[1000px]"
 				components={{
 					code({ node, inline, className, children, ...props }) {
 						const match = /language-(\w+)/.exec(className || "");
