d3317a339ac6b4081797a6f35372982f9a0c6f4d
francis <franzdiaz460@gmail.com>
2023-06-22 07:16:47 +0400

    style: adjusted font size

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -27,7 +27,7 @@ const Editor = ({ onChange, input }: IEditorProps) => {
 			theme={editorAppTheme}
 			value={input}
 			onChange={onChange}
-			className="markdown-editor font-semibold focus-within:border-t-[3px] text-sm focus-within:border-vn-blue border-solid outline-none flex-1 flex justify-center "
+			className="markdown-editor font-medium focus-within:border-t-[3px] text-[.8rem] focus-within:border-vn-blue border-solid outline-none flex-1 flex justify-center "
 			height="100%"
 			width="100%"
 			extensions={[
