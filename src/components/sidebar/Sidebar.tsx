f9e5dc98131d0997fa2d434663fa702a999fb56f
francis <franzdiaz460@gmail.com>
2023-06-23 07:50:15 +0400

    style: for sidebar to accomodate small layouts

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -10,7 +10,7 @@ const Sidebar = ({ children, sideBarRef }: ISidebarProps) => {
 		<div
 			id="sidebar"
 			ref={sideBarRef}
-			className="sidebar-inactive z-10 bg-vn-black flex flex-col"
+			className="sidebar-inactive z-10 bg-vn-black flex flex-col max-[500px]:absolute max-[500px]:left-10 h-full "
 		>
 			{children}
 		</div>
