e5660932f5f910e33e92566f4c7aa3dfb0bd653c
francis <franzdiaz460@gmail.com>
2023-06-29 15:37:57 +0400

    something

--- src/utils/routes.tsx
+++ src/utils/routes.tsx
@@ -9,57 +9,64 @@ import SignUp from "@/components/auth/SignUp";
 import PasswordRecovery from "@/components/auth/PasswordRecovery";
 import ErrorPage from "@/pages/ErrorPage";
 
-export const ROUTES = createBrowserRouter([
-	{
-		path: "/",
-		element: <RootAuth />,
-		children: [
-			{
-				path: "sign-up",
-				element: <SignUp />,
-			},
-			{
-				path: "sign-in",
-				element: <SignIn />,
-			},
-			{
-				path: "forgot-password",
-				element: <PasswordRecovery />,
-			},
-		],
-	},
-	{
-		path: "/app",
-		element: <App />,
-		shouldRevalidate: () => false,
-		errorElement: <ErrorPage code="404" redirect={true} />,
-		children: [
-			{
-				path: ":noteID",
-				element: <Note />,
-				errorElement: <ErrorPage code="404" redirect={true} />,
-				loader: ({ request, params }) => {
-					let trimUrl = request.url.slice(-16);
-					console.log(params);
-					if (trimUrl !== params.noteID) {
-						console.log("redirect");
-						throw new Response("Not Found", { status: 404 });
-					}
-					return null;
+export const ROUTES = createBrowserRouter(
+	[
+		{
+			path: "/",
+			element: <RootAuth />,
+			children: [
+				{
+					path: "sign-up",
+					element: <SignUp />,
 				},
-			},
-			{
-				path: "empty-notes",
-				element: <EmptyNotes />,
-			},
-			{
-				path: "vim-cheatsheet",
-				element: <VimnoteCheatSheet />,
-			},
-		],
-	},
-	{
-		path: "*",
-		element: <ErrorPage code="404" redirect={true} />,
-	},
-]);
+				{
+					path: "sign-in",
+					element: <SignIn />,
+				},
+				{
+					path: "forgot-password",
+					element: <PasswordRecovery />,
+				},
+			],
+		},
+		{
+			path: "/app",
+			element: <App />,
+			shouldRevalidate: () => false,
+			errorElement: <ErrorPage code="404" redirect={true} />,
+			children: [
+				{
+					path: ":noteID",
+					element: <Note />,
+					// errorElement: <ErrorPage code="404" redirect={true} />,
+					// loader: ({ request, params }) => {
+					// 	let trimUrl = request.url.slice(-16);
+					// 	console.log(params);
+					// 	if (trimUrl !== params.noteID) {
+					// 		console.log("redirect");
+					// 		throw new Response("Not Found", { status: 404 });
+					// 	}
+					// 	return null;
+					// },
+				},
+				{
+					path: "empty-notes",
+					element: <EmptyNotes />,
+				},
+				{
+					path: "vim-cheatsheet",
+					element: <VimnoteCheatSheet />,
+				},
+				{
+					path: "*",
+					element: <ErrorPage code="404" redirect={true} />,
+				},
+			],
+		},
+		{
+			path: "*",
+			element: <ErrorPage code="404" redirect={true} />,
+		},
+	],
+	{ basename: "/" }
+);
